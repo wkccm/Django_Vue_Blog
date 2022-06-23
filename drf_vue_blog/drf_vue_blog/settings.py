91e111b9933cbe7fbd439e94d445cca65a0de413
wkccm <1073385724@qq.com>
2022-06-23 22:10:42 +0800

    update static path

--- drf_vue_blog/drf_vue_blog/settings.py
+++ drf_vue_blog/drf_vue_blog/settings.py
@@ -17,7 +17,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, "frontend/dist/"),
+    os.path.join(BASE_DIR, "../frontend/dist/"),
 ]
 
 STATIC_ROOT=os.path.join(BASE_DIR, 'collected_static')
